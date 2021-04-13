db11bebdac87cfee8d3307f0a127aac2ec46b9cf
DysonY <dysonye1632@gmail.com>
2021-04-13 16:54:52 -0400

    lexer

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ This program implements a solver for the boolean satisfiability problem.
 # Changelog
 4/10/21: implemented evaluation of boolean functions for specific inputs  
 4/11/21: implemented verification of satisfiability  
-4/12/21: fixed error in count_vars; updated tests
+4/12/21: fixed error in count_vars; updated tests  
+4/13/21: added lexer
--- lex.ml
+++ lex.ml
@@ -3,6 +3,13 @@ type token =
   | LPAREN | RPAREN
   | NOT | AND | OR
 
+let explode (str : string) : char list =
+  let rec loop index output =
+    if index < 0 then output
+    else loop (index - 1) (str.[index] :: output)
+  in
+  loop (String.length str - 1) []
+
 let collapse (int_chars : char list) : token list =
   if int_chars = [] then []
   else
@@ -15,7 +22,8 @@ let collapse (int_chars : char list) : token list =
     in
     [NUM (loop int_chars "")]
 
-let tokenize (stream : char list) : token list =
+let tokenize (s : string) : token list =
+  let stream = explode s in
   let rec loop chars tokens int_chars =
     match chars with
     | [] -> tokens @ (collapse int_chars)
@@ -24,6 +32,7 @@ let tokenize (stream : char list) : token list =
     | '~' :: cs -> loop cs (tokens @ (collapse int_chars) @ [NOT])    []
     | '&' :: cs -> loop cs (tokens @ (collapse int_chars) @ [AND])    []
     | '|' :: cs -> loop cs (tokens @ (collapse int_chars) @ [OR])     []
+    | ' ' :: cs -> loop cs (tokens @ (collapse int_chars) @ [OR])     []
     | c :: cs when Char.code c > 47 && Char.code c < 58 ->
       loop cs tokens (int_chars @ [c])
     | c :: _ -> failwith ("invalid character: " ^ (String.make 1 c))
