ccdf0e7dd46e9bc42070f66cae07b0ab32d56c4b
DysonY <dysonye1632@gmail.com>
2021-04-13 17:01:02 -0400

    lex whitespace skip

--- lex.ml
+++ lex.ml
@@ -3,6 +3,7 @@ type token =
   | LPAREN | RPAREN
   | NOT | AND | OR
 
+(* Explode a string into a list *)
 let explode (str : string) : char list =
   let rec loop index output =
     if index < 0 then output
@@ -10,6 +11,7 @@ let explode (str : string) : char list =
   in
   loop (String.length str - 1) []
 
+(* Join a list of int characters into a NUM token *)
 let collapse (int_chars : char list) : token list =
   if int_chars = [] then []
   else
@@ -22,6 +24,7 @@ let collapse (int_chars : char list) : token list =
     in
     [NUM (loop int_chars "")]
 
+(* Generate a list of tokens from an input string *)
 let tokenize (s : string) : token list =
   let stream = explode s in
   let rec loop chars tokens int_chars =
@@ -32,7 +35,8 @@ let tokenize (s : string) : token list =
     | '~' :: cs -> loop cs (tokens @ (collapse int_chars) @ [NOT])    []
     | '&' :: cs -> loop cs (tokens @ (collapse int_chars) @ [AND])    []
     | '|' :: cs -> loop cs (tokens @ (collapse int_chars) @ [OR])     []
-    | ' ' :: cs -> loop cs (tokens @ (collapse int_chars) @ [OR])     []
+    | '\n' :: cs | '\t' :: cs | ' ' :: cs ->
+      loop cs (tokens @ (collapse int_chars)) []
     | c :: cs when Char.code c > 47 && Char.code c < 58 ->
       loop cs tokens (int_chars @ [c])
     | c :: _ -> failwith ("invalid character: " ^ (String.make 1 c))
